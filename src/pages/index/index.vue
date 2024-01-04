0866e0799db02e47b984730072d16f85d3d136a3
Masin_zzq <masin2001zzq@163.com>
2024-01-04 15:48:05 +0800

    feat: import uni-ui

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,7 +3,14 @@
 </script>
 
 <template>
-  <view class="index">index</view>
+  <view class="index">
+    <uni-card
+      title="基础卡片"
+      @click=""
+    >
+      首页
+    </uni-card>
+  </view>
 </template>
 
 <style lang="scss">
